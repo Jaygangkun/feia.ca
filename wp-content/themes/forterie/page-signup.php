85c1ce68637df9fa83cbd331b6d216de432f7399
JayGangkun <jaygangkun@hotmail.com>
2021-05-06 09:26:07 -0400

    integrate growsurf

--- wp-content/themes/forterie/page-signup.php
+++ wp-content/themes/forterie/page-signup.php
@@ -15,6 +15,9 @@ get_header();
         display: none;
     }
 </style>
+<script type="text/javascript">
+  (function(g,r,s,f){g.grsfSettings={campaignId:"dq98lx",version:"2.0.0"};s=r.getElementsByTagName("head")[0];f=r.createElement("script");f.async=1;f.src="https://app.growsurf.com/growsurf.js"+"?v="+g.grsfSettings.version;f.setAttribute("grsf-campaign", g.grsfSettings.campaignId);!g.grsfInit?s.appendChild(f):"";})(window,document);
+</script>
 <div class="tech101signup-page-content">
     <div class="tech101signup-page-content-wrap">
         <div class="tech101signup-content-left" style="background-image:url(<?php echo get_template_directory_uri()?>/images/tech101signup-image-new.png);">
@@ -66,55 +69,57 @@ get_header();
             <div class="tech101signup-step" step="1">
                 <h2 class="tech101signup-title">Join Tech 101 today, <br>100% free.</h2>
                 <p class="tech101signup-desc">Created by Fort Erie International Academy and Huawei Technologies, Tech 101 was designed to make digital literacy accessible to all Canadians.</p>
-                <div class="flex-row">
-                    <div class="flex-col-50">
-                        <div class="tech101signup-form-group">
-                            <label>First Name</label>
-                            <div class="tech101signup-form-input-wrap">
-                                <input type="text" id="first_name">
+                <form name="tech101signup_form">
+                    <div class="flex-row">
+                        <div class="flex-col-50">
+                            <div class="tech101signup-form-group">
+                                <label>First Name</label>
+                                <div class="tech101signup-form-input-wrap">
+                                    <input type="text" id="first_name" name="first_name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex-col-50">
+                            <div class="tech101signup-form-group">
+                                <label>Last Name</label>
+                                <div class="tech101signup-form-input-wrap">
+                                    <input type="text" id="last_name" name="last_name">
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="flex-col-50">
-                        <div class="tech101signup-form-group">
-                            <label>Last Name</label>
-                            <div class="tech101signup-form-input-wrap">
-                                <input type="text" id="last_name">
-                            </div>
+                    <div class="tech101signup-form-group">
+                        <label>Username</label>
+                        <div class="tech101signup-form-input-wrap">
+                            <input type="text" id="username" name="username">
                         </div>
                     </div>
-                </div>
-                <div class="tech101signup-form-group">
-                    <label>Username</label>
-                    <div class="tech101signup-form-input-wrap">
-                        <input type="text" id="username">
+                    <div class="tech101signup-form-group">
+                        <label>Email</label>
+                        <div class="tech101signup-form-input-wrap">
+                            <input type="text" id="email" name="email">
+                        </div>
                     </div>
-                </div>
-                <div class="tech101signup-form-group">
-                    <label>Email</label>
-                    <div class="tech101signup-form-input-wrap">
-                        <input type="text" id="email">
+                    <div class="tech101signup-form-group">
+                        <label>Postal Code (Optional)</label>
+                        <div class="tech101signup-form-input-wrap">
+                            <input type="text" id="post_code" name="post_code">
+                        </div>
                     </div>
-                </div>
-                <div class="tech101signup-form-group">
-                    <label>Postal Code (Optional)</label>
-                    <div class="tech101signup-form-input-wrap">
-                        <input type="text" id="post_code">
-                    </div>
-                </div>
 
-                <div class="tech101signup-form-group">
-                    <label class="container">Yes, I would like to receive exclusive opportunities, community updates, career advice, and more through the Tech 101 Newsletter!
-                        <input type="checkbox" checked>
-                        <span class="checkmark"></span>
-                    </label>
-                </div>
-                <div class="tech101signup-form-group tech101signup-form-group-submit-btn">
-                    <span class="tech101signup-form-btn" id="tech101signup_next_btn">Next</span>
-                    <div>
-                    <span class="tech101signup-desc">Already have an account?</span><a class="tech101signup-form-link" href="https://tech101.feia.ca/login">Login</a>
+                    <div class="tech101signup-form-group">
+                        <label class="container">Yes, I would like to receive exclusive opportunities, community updates, career advice, and more through the Tech 101 Newsletter!
+                            <input type="checkbox" checked name="receive_update">
+                            <span class="checkmark"></span>
+                        </label>
                     </div>
-                </div>
+                    <div class="tech101signup-form-group tech101signup-form-group-submit-btn">
+                        <span class="tech101signup-form-btn" id="tech101signup_next_btn">Next</span>
+                        <div>
+                        <span class="tech101signup-desc">Already have an account?</span><a class="tech101signup-form-link" href="https://tech101.feia.ca/login">Login</a>
+                        </div>
+                    </div>
+                </form>
             </div>
             <div class="tech101signup-step" step="2" style="display: none">
                 <h2 class="tech101signup-title">You're ready to begin!</h2>
@@ -143,7 +148,7 @@ get_header();
     jQuery(document).ready(function(){
         
         jQuery(document).on('click', '#tech101signup_next_btn', function(){
-
+            
             if(jQuery('#first_name').val() == ''){
                 alert('Please Input First Name');
                 jQuery('#first_name').focus();
@@ -174,6 +179,10 @@ get_header();
                 return;
             }
 
+            if (window.growsurf) {
+                growsurf.triggerReferral(jQuery('#email').val());
+            }
+            
             jQuery.ajax({
                 url: '<?php echo admin_url('admin-ajax.php')?>',
                 type: 'post',
